e7f489c7eb87a795ad39431720f2f36c8abd0054
Boronus <boronus@gmail.com>
2018-01-25 15:41:14 +0700

    + JS in functions

--- script/application.js
+++ script/application.js
@@ -1,23 +1,31 @@
 console.log("LOL");
 
-let palindromeInputElem = document.getElementById('palindrome_text');
+let palindromeInputElem = document.getElementById('palindrome_textarea');
 let palindromeResultElem = document.getElementById('palindrome_result');
+var filterRegExp = new RegExp(/[^0123456789А-ЯЁа-яёA-Za-z]/g);
+
 let palindromeFilteredElem = document.getElementById('palindrome_filtered');
 
-palindromeInputElem.addEventListener('input', function (e) {
-    let palindromeFiltered = this.value
-        .toLowerCase()
+palindromeInputElem.addEventListener('input', checkPalindrome);
+
+function checkPalindrome() {
+    palindromeResultElem.innerText = returnAnswer(this.value);    
+}
+
+function returnAnswer(inputText) {
+    if (!inputText) return 'Палиндром не введён';
+    //let palindromeFiltered = filter(this.value)
+    //palindromeFilteredElem.innerText = palindromeFiltered;
+    return isPalindrome(filter(inputText)) ? 'Палиндром' : 'Не палиндром';    
+}
+
+function filter(str) {
+    return str.toLowerCase()
         .replace( 'ё', 'е' )
-        .replace( /[^0123456789А-ЯЁа-яёA-Za-z]/g, '' );
-    palindromeFilteredElem.innerText = palindromeFiltered;
-    palindromeResultElem.innerText = isPalindrome(palindromeFiltered) ? 'Палиндром' : 'Не палиндром';    
-});
-
-function filterRu() {
-
+        .replace( filterRegExp, '' );
 }
 
 function isPalindrome(str) {
     strReverse = str.split('').reverse().join('');
-    return strReverse == str;
+    return strReverse === str;
 }
